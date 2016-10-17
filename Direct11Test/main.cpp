6cddd30571f5463779d62aaaad1ab760a2e2ee3f
ohcumt <1040183251@qq.com>
2016-10-17 15:47:55 +0800

    创建空白窗口

---
+++ Direct11Test/main.cpp
@@ -0,0 +1,117 @@
+
+
+#include <windows.h>
+
+
+LRESULT CALLBACK WndProc(
+	HWND hwnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam);
+
+int WINAPI wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+
+
+	// 窗口的注册和创建
+
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX); //该结构体大小
+	wndClass.style = CS_HREDRAW | CS_VREDRAW; // 窗口风格
+	wndClass.lpfnWndProc = WndProc; // 回调函数句柄
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.lpszMenuName = "MenuName";
+	wndClass.lpszClassName = "DX11BookWindowClass";
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+
+	// 设置窗口尺寸
+	RECT rc = { 0,0,640,480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND hwnd = CreateWindowA(
+		"DX11BookWindowClass", // 类名字
+		"Blank Win32 window", // 窗口名称
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,
+		rc.bottom - rc.top,
+		NULL, NULL, hInstance, NULL
+	);
+
+	if (!hwnd)
+	{
+		return -1;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return static_cast<int>(msg.wParam);
+
+}
+
+
+LRESULT CALLBACK WndProc(
+	HWND hwnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam)
+{
+	PAINTSTRUCT paintStruct;
+	HDC hDC;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hDC = BeginPaint(hwnd, &paintStruct);
+		EndPaint(hwnd, &paintStruct);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
